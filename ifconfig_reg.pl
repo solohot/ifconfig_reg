38137561f0776aed48ed4bb9980f2dbb2275110a
solohot <all4you@aliyun.com>
2016-03-30 17:02:46 +0800

    Maybe,you also want to know the IPv6 Address.

--- ifconfig_reg.pl
+++ ifconfig_reg.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
-@nainfo = `ifconfig | awk '/Link encap|inet addr/'`;#ifconfig output.
+@nainfo = `ifconfig | awk '/Link encap|inet addr|inet6 addr/'`;#ifconfig output.
 print "Network Interface";
 print " "x4;
-print "IPv4 Address\n";
+print "IPv4 Address";
+print " "x4;
+print "IPv6 Address\n";
+
 foreach(@nainfo){
 	#Get Network Interface.
 	if(/^\b(\w+)\b/){
@@ -10,6 +13,10 @@ foreach(@nainfo){
 	}
 	#Get IPv4 Address.
 	if(/inet addr:((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))/){
-		print "$1\n";
+		printf "%-16s\n",$1;
+	}
+	#Get IPv6 Address.
+	if(/inet addr:((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))/){
+		printf "%s\n",$1;
 	}
 }
--- readme.txt
+++ readme.txt
@@ -1,5 +1,9 @@
 This Perl project can get the output of 'ifconfig' and return the hash table whose key is the network adapter, and value is ip address.
 
+Version 0.2
+Time:Wed Mar 30 16:58:34 HKT 2016
+Note:Maybe,you also want to know the IPv6 Address. 
+
 Version 0.1
 Time:Wed Mar 30 16:04:41 HKT 2016
 Note:All function test success.You can use the program to get the hash of Network Interface and IPv4 Address.
